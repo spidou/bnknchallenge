21caeeb485a4ce91fbe6fe313a0ae394b6206baa
Mathieu Fontaine <spidou@gmail.com>
2018-01-14 11:30:27 +0400

    Optimize code and performance of the scraper (better, faster, stronger)

--- app.js
+++ app.js
@@ -2,16 +2,71 @@ var scraper = require('./lib/scraper')
 var url = 'https://web.bankin.com/challenge/index.html'
 // console.log(url)
 
-var onPageReady = function(page, done) {
+var startedAt = new Date()
+var endedAt = null
+
+var page = null
+var doc = null
+
+var onPageReady = function(_page, done) {
+  page = _page
   // console.log('onPageReady')
+  click(function() {
+    wait(200, function() {
+      checkTable(function(length) {
+        if (length > 0) {
+          getResults(function(result) {
+            if (!result) {
+              onFinish("An error has occured")
+            } else {
+              onFinish(null, result)
+            }
+            done()
+          })
+        } else {
+          onPageReady(page, done)
+        }
+      })
+    })
+  })
+}
+
+var click = function(callback) {
+  // console.log('click on button')
   page.evaluate(function () {
     var btn = document.getElementById('btnGenerate')
-    $(btn).click()
+    btn && btn.click()
+  }, callback)
+}
 
+var wait = function(time, callback) {
+  // console.log('wait ' + time + ' ms')
+  setTimeout(callback, time);
+}
+
+var checkTable = function(callback) {
+  // console.log('check table')
+  page.evaluate(function () {
+    if (document.getElementById('fm')) {
+      console.log('table in iframe')
+      doc = document.getElementById('fm').contentWindow.document
+    } else {
+      console.log('table in document')
+      doc = document
+    }
+    return doc.querySelectorAll('table tr').length
+  }, function(result) {
+    callback(result)
+  })
+}
+
+var getResults = function(callback) {
+  // console.log('getResults')
+  page.evaluate(function () {
     var objects = []
-    var trs = document.querySelectorAll('#dvTable tr')
+    var trs = doc.querySelectorAll('table tr')
     if (trs.length) {
-      for (i = 0, len = trs.length; i < len; i++) {
+      for (i = 1, len = trs.length; i < len; i++) { // start at 1 to exclude table head
         var tr = trs[i]
         objects.push({
           account: tr.cells[0].outerText,
@@ -21,18 +76,10 @@ var onPageReady = function(page, done) {
         })
       }
     }
-
-    return {
-      btn: btn,
-      objects: objects
-    }
+    // console.log(objects.length + ' objects found')
+    return objects
   }, function (result) {
-    if (!result) {
-      onFinish("An error has occured")
-    } else {
-      onFinish(null, result)
-    }
-    done()
+    callback(result)
   })
 }
 
@@ -41,15 +88,19 @@ var onError = function (err) {
   console.log(err)
 }
 
-var onFinish = function (err, result) {
-  console.log('onFinish')
+var onFinish = function (err, lines) {
+  // console.log('onFinish')
   if (err) {
     console.log(err)
     return process.exit(1)
   }
-  // console.log(result)
-  if (result.objects.length) {
-    console.log(result.objects)
+  // console.log(lines)
+  var len = lines.length
+  if (len) {
+    // console.log(lines) // display lines on console
+    
+    var endedAt = new Date()
+    console.log(len + " results found in " + (endedAt - startedAt) / 1000 + " seconds")
     process.exit()
   } else {
     console.log('Page returns 0 object. Let\'s retry!')
